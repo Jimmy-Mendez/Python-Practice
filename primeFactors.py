7066a25e773a714373ed4f2136feda3f48cab9b5
jimmy <jm18f@my.fsu.edu>
2021-01-13 15:09:53 -0500

    find primes

--- primeFactors.py
+++ primeFactors.py
@@ -1,2 +1,17 @@
-num = input("Enter a number")
-print(num)
+
+def isPrime(number):
+    i = 2
+    while i < number:
+        if number%i == 0:
+            return 0
+        i+=1
+    return 1
+
+while(1):
+
+    num = int (input("Enter a number: "))
+
+    if (isPrime(num) == 1):
+        print("prime!")
+    else:
+        print("not prime!")
