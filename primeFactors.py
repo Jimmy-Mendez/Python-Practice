98a2ea619edc045d4f1b30b115ec89a7b465a680
jimmy <jm18f@my.fsu.edu>
2021-01-14 15:52:48 -0500

    Prime factors given

--- primeFactors.py
+++ primeFactors.py
@@ -8,21 +8,21 @@ def isPrime(number):
     return 1
 
 def primeFactors(number):
+    ans = []
     if (isPrime(number) == 1 ):
         ans = [1,number]
         print(ans)
-    i = 2
-    if (isPrime(i)):
-
-
-
+    i = number - 1
+    while(i!=1):
+        if (isPrime(i)):
+            if(number%i == 0):
+                ans.append(i)
+        i-=1
+    return ans
 
 
 while(1):
-
-    num = int (input("Enter a number: "))
-
-    if (isPrime(num) == 1):
-        print("prime!")
-    else:
-        print("not prime!")
+    inp = int(input("enter a number: "))
+    result = primeFactors(inp)
+    print("the prime factors are: ")
+    print(result)
