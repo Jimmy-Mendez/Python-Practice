dbbb1b32f721712ac663ffb0ba4b6233026f44d6
Jimmy Mendez <54858901+Jimmy-Mendez@users.noreply.github.com>
2021-01-19 02:00:57 -0500

    Update Grades.py

--- Grades.py
+++ Grades.py
@@ -1,24 +1,32 @@
-import math
-
 
 class Course:
-    def __init__(self, components, componentWeights):
+    
+    def __init__(self, name, components, componentWeights):
+        self.n = name
         self.c = components
         self.w = componentWeights
-        grades = []
+
     def calcCurrentGrade(self):
+        count = 0
+        grades = []
         completedAssignments = 0
-        grade = 0
+        grade = 0.0
         fakeGrade = 0
-        for i in self.components:
-            print("enter the grade for {} : ", self.components[i])
-            self.grades[i] = input()
-            if self.grades[i] != 0
-                completedAssignments++
-                fakeGrade += self.componentWeights[i] * 100
-            grade += self.grades[i] * self.componentWeights[i]
-        print("Your current grade assuming 0s for the rest of assignments is: {}", grade)
-        print("Your current grade assuming 100s for the rest of assignments is: {}", grade)
+        points_lost = 0
+        for i in self.c:
+            print("enter the grade for: ", i)
+            grades.append(float(input()))
+            if grades[count] != 0:
+                completedAssignments+=1
+                points_lost += (self.w[count] * 100)-(grades[count] * self.w[count])
+            else:
+                fakeGrade += self.w[count] * 100
+            grade += grades[count] * self.w[count]
+            count+=1
+        fakeGrade = fakeGrade + grade
+        print("Your current grade assuming 0s for the rest of assignments is: ", grade)
+        print("Your current grade assuming 100s for the rest of assignments is: ", fakeGrade)
+        print("points lost so far:", points_lost)
 
 
 
@@ -34,7 +42,7 @@ class Course:
 
 #A = 93
 
-CAP5619 = Course(["Attendance", "Project 1", "Project 2", "Homework", "Midterm, Final Project"],[.10,.10,.10,.25,.30,.15])
+CAP5619 = Course("CAP 5619", ["Attendance", "Project 1", "Project 2", "Homework", "Midterm", "Final Project"],[.10,.10,.10,.25,.30,.15])
 
 ###################################################################
 
@@ -46,6 +54,8 @@ CAP5619 = Course(["Attendance", "Project 1", "Project 2", "Homework", "Midterm, 
 #35% Final Exam
 
 #A = 85
+
+ECO3101 = Course("ECO 3101", ["Quizzes", "Midterm 1", "Midterm 2", "Final"],[.25,.20,.20,.35])
 
 
 #################################################################
@@ -61,6 +71,7 @@ CAP5619 = Course(["Attendance", "Project 1", "Project 2", "Homework", "Midterm, 
 
 #A = 93
 
+MAC2313 = Course("MAC 2313", ["Homework", "Discussions", "Exam 1", "Exam 2", "Exam 3", "Exam 4", "Final"],[.35,.10,.10,.10,.10,.10,.15])
 
 ################################################################
 
@@ -74,9 +85,19 @@ CAP5619 = Course(["Attendance", "Project 1", "Project 2", "Homework", "Midterm, 
 
 #A = 93
 
-
+MAS3105 = Course("MAS 3105", ["Homework", "Quizzes", "Test 1", "Test 2", "Final"],[.18,.15,.20,.22,.25])
 
 ##########################################
 
 
+CourseList = [CAP5619, ECO3101, MAC2313, MAS3105]
+
 courseName = input("enter a course number: ")
+
+course = Course("N/A",[],[])
+
+for i in CourseList:
+    if courseName == i.n:
+        course = i
+
+course.calcCurrentGrade()
